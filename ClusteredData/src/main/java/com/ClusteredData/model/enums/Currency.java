f7fc94c01dae45bcc71adebee37fe0aee20d1814
MutasemShboul <mutasemshboul14@gmail.com>
2023-10-13 22:48:32 +0300

    update currency

--- ClusteredData/src/main/java/com/ClusteredData/model/enums/Currency.java
+++ ClusteredData/src/main/java/com/ClusteredData/model/enums/Currency.java
@@ -3,34 +3,30 @@ package com.ClusteredData.model.enums;
 import java.util.stream.Stream;
 
 public enum Currency {
-    USD("USD", "United States Dollar", "$"),
-    EUR("EUR", "Euro", "€"),
-    GBP("GBP", "British Pound Sterling", "£"),
-    JPY("JPY", "Japanese Yen", "¥"),
-    AUD("AUD", "Australian Dollar", "A$"),
-    CAD("CAD", "Canadian Dollar", "C$");
+    JOR("JOR"),
+    USD("USD"),
+    GBP("GBP"),
+    JPY("JPY"),
+    AUD("AUD"),
+    EUR("EUR"),
+    AFN("AFN"),
+    BHD("BHD"),
+    BRL("BRL"),
+    CAD("CAD");
 
     private final String code;
-    private final String name;
-    private final String symbol;
 
-    Currency(String code, String name, String symbol) {
+
+    Currency(String code) {
         this.code = code;
-        this.name = name;
-        this.symbol = symbol;
+
     }
 
     public String getCode() {
         return code;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public String getSymbol() {
-        return symbol;
-    }
 
     public static Stream<Currency> stream() {
         return Stream.of(Currency.values());
